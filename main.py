575d84fba2f5ee848b8d423c384d9c8109036442
Sankar, Natarajan <sankar.natarajan@catamilacademy.org>
2021-05-27 20:42:10 -0700

    Initial version

---
+++ main.py
@@ -0,0 +1,112 @@
+from flask import Flask, request, render_template
+import os
+import random
+import redis
+import socket
+import sys
+import logging
+from datetime import datetime
+from opencensus.ext.azure import metrics_exporter
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.tracer import Tracer
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+
+# App Insights
+# TODO: Import required libraries for App Insights
+
+# Logging
+logger = logging.getLogger(__name__)
+# Metrics
+exporter = metrics_exporter.new_metrics_exporter(enable_standard_metrics=True,connection_string='InstrumentationKey=311d5e15-9119-45f2-8ec0-62e88965b17b;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/')
+
+# Tracing
+tracer = Tracer(exporter=AzureExporter(connection_string='InstrumentationKey=311d5e15-9119-45f2-8ec0-62e88965b17b;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/'),sampler=ProbabilitySampler(1.0),)
+
+app = Flask(__name__)
+
+# Requests
+middleware = FlaskMiddleware(app,exporter=AzureExporter(
+                        connection_string='InstrumentationKey=311d5e15-9119-45f2-8ec0-62e88965b17b;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/'),
+                        sampler=ProbabilitySampler(1.0),)
+
+# Load configurations from environment or config file
+app.config.from_pyfile('config_file.cfg')
+
+if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
+    button1 = os.environ['VOTE1VALUE']
+else:
+    button1 = app.config['VOTE1VALUE']
+
+if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
+    button2 = os.environ['VOTE2VALUE']
+else:
+    button2 = app.config['VOTE2VALUE']
+
+if ("TITLE" in os.environ and os.environ['TITLE']):
+    title = os.environ['TITLE']
+else:
+    title = app.config['TITLE']
+
+# Redis Connection
+r = redis.Redis()
+
+# Change title to host name to demo NLB
+if app.config['SHOWHOST'] == "true":
+    title = socket.gethostname()
+
+# Init Redis
+if not r.get(button1): r.set(button1,0)
+if not r.get(button2): r.set(button2,0)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'GET':
+
+        # Get current values
+        vote1 = r.get(button1).decode('utf-8')
+        tracer.span(name="Cat vote")
+
+        vote2 = r.get(button2).decode('utf-8')
+        tracer.span(name="Dog vote")
+
+        # Return index with values
+        return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+
+    elif request.method == 'POST':
+
+        if request.form['vote'] == 'reset':
+
+            # Empty table and return results
+            r.set(button1,0)
+            r.set(button2,0)
+            vote1 = r.get(button1).decode('utf-8')
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            logger.info("Cats vote : {}",vote1)
+
+            vote2 = r.get(button2).decode('utf-8')
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            logger.info("Dog vote : {}",vote2)
+
+            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+
+        else:
+
+            # Insert vote result into DB
+            vote = request.form['vote']
+            r.incr(vote,1)
+
+            # Get current values
+            vote1 = r.get(button1).decode('utf-8')
+            vote2 = r.get(button2).decode('utf-8')
+
+            # Return results
+            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+
+if __name__ == "__main__":
+    # comment line below when deploying to VMSS
+    #app.run() # local
+    # uncomment the line below before deployment to VMSS
+    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
